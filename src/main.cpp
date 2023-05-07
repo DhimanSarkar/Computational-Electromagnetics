038f28ff17ce4a8d81b04cd76fc5654b8a9d1b3e
Dhiman Sarkar <dhiman.eminem@gmail.com>
2023-05-07 15:01:40 +0530

    MoM finished

--- src/main.cpp
+++ src/main.cpp
@@ -1,38 +1,47 @@
+/*
+#############################################################################################
+#                            Computational Electromagnetics                                 #
+#  Implementation of Method of Moment (MoM) to solve Electrostatic Lince Charge Problem     #
+#############################################################################################
+*/
+
 #include<iostream>
-#include "Fastor/Fastor.h"
+#include "Eigen/Eigen"
 
 int main(){
-    std::system("cls"); // Clear Previous Output Screen
-    std::cout<<"MoM"<<std::endl;
-    
-    const int N = 5;        //No of elements
-    float L = 2;         //Length of wire
-    float delX = L/N;  //Grid length
+    std::system("cls"); //console>cls
+  
+    const int N = 100;       //No of elements
+    float L = 2;            //Length of wire
+    float delX = L/N;       //Grid length
 
-    float eps0 = 8.8541878128e-12; //Vacume permitivity
+    float eps0 = 8.8541878128e-12;  //Vacume permitivity
     float eps = 1;                  //Relative Permitivity of the Material
     float r = 0.0001;               //Radius of the wire
 
-    Fastor::Tensor<float,N,1> B;   //Defining Solution matrix "B" of AX=B
-    B.fill(4*3.14*eps0*eps);        //fill all the elements with 2.5
+    Eigen::Matrix<double,N,1> B;    //Defining Solution matrix "B" of AX=B
+    B.setConstant(4*3.14*eps0*eps); //fill all the elements with 4*pi*Epsilon
 
-    Fastor::Tensor<float,N,N> A;   //Defining Solution matrix "A" of AX=B
-    A.ones();                       // fill all the elements with 2.5
-    int i,j;
-    i=j=0;
-    for(i=0; i<N; i++)
+    Eigen::Matrix<double,N,N> A;    //Defining Solution matrix "A" of AX=B
+A.setOnes();                        //Init
+    for(int i=0; i<N; i++)
     {
-        for (j=0; j<N; j++)
+        for(int j=0; j<N; j++)
         {
             int m=i+1; //CPP index -> Matrix index
             int n=j+1; //CPP index -> Matrix index
             float x_i = (m-0.5)*delX; //locations of Testing Function (In this case, middle of every node)
             A(i,j) = std::log(std::abs((n*delX - x_i + std::sqrt(std::pow((n*delX - x_i),2)+r*r))/((n-1)*delX - x_i + std::sqrt(std::pow(((n-1)*delX - x_i),2)+r*r))));
-        }    
+        }
     }
+    
+    Eigen::Matrix<double,N,N> A_inv;
+    A_inv = A.inverse();
 
-    Fastor::Tensor<float,N> X = Fastor::solve(A,B);    //Solving for "X" in AX+B
-    Fastor::print(X);
+    Eigen::Matrix<double,N,1> X;    //Defining Solution matrix "X" of AX=B
+    X = A_inv * B;                  //Solving for "X" in AX+B
+
+    std::cout<<X<<std::endl;
 
     return 0;
-}
+    }
