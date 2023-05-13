e5e56da65598e9bc41d79a85ca797dbdd02f84e2
Dhiman Sarkar <dhiman.eminem@gmail.com>
2023-05-13 22:47:00 +0530

    test Case1

--- src/test1.m
+++ src/test1.m
@@ -1,26 +1,39 @@
-clear;clc;close all;
+clear;clc;
 
-delX = 1/51;
+N=100;
+t_max = 300;
+
+delX = 1/N;
 delT = 3e8/delX;
-x = linspace(0,1,51);
+xE = linspace(0,1,N);
+xH = xE(1:end-1);
+xH = xH + delX/2;
 xx = linspace(0,1,1000);
 yy = sin(2.*3.141519./.3.*abs(xx-.5));
 
 E = importdata("../build/Debug/fieldE.txt");
 H = importdata("../build/Debug/fieldH.txt");
 
-% Timestamp: 0
-figure;
+% Movie
+M=moviein(t_max);
 
-subplot(2,1,1);
-grid on; hold on;
-stem(x,E(1,:));
-plot(xx,yy);
-xlim([0,1]);
-
-subplot(2,1,2);
-grid on; hold on;
-stem((x+delX/2),H(1,:));
-xlim([0,1]);
-
-
+for t = 1:t_max
+    plot3(0*xE,xE,E(t,:));
+    hold on;
+    plot3(H(t,:),xH,0*xH);
+    hold off;
+    grid on;
+    xlabel("H_y");
+    ylabel("Time");
+    zlabel("E_x");
+    zlim([-2 2]);
+    xlim([-2,2]);
+    ylim([0 1]);
+    M(:,t) = getframe();
+    
+end
+v = VideoWriter('newfile.avi');
+open(v);
+writeVideo(v,M)
+close(v)
+movie(M,1);
